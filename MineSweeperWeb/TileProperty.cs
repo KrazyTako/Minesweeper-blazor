30c4ac6d9e8cad3f543baa4c84752c90fe453a3b
David Rodriguez <krazytako@hotmail.com>
2020-06-02 20:12:35 -0700

    Add project files.

---
+++ MineSweeperWeb/TileProperty.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MineSweeperWeb
+{
+    public class TileProperty
+    {
+        public bool IsBomb { get; set; }
+        public bool IsRevealed { get; set; }
+        public string BackgroundColor { get; set; } = "lightgrey";
+        public string Message { get; set; }
+        public int Row { get; set; }
+        public int Col { get; set; }
+    }
+}
