3a086e724d62c6d294adcd775af1f7696c14a352
David Rodriguez <krazytako@hotmail.com>
2020-06-02 23:24:24 -0700

    bunch of updates

--- MineSweeperWeb/TileProperty.cs
+++ MineSweeperWeb/TileProperty.cs
@@ -10,8 +10,31 @@ namespace MineSweeperWeb
     {
         public bool IsBomb { get; set; }
         public bool IsRevealed { get; set; }
+        public bool IsFlagged { get; set; }
         public string BackgroundColor { get; set; } = "lightgrey";
-        public string Message { get; set; }
+        public string Message
+        {
+            get
+            {
+                if(IsFlagged)
+                {
+                    return "?";
+                }
+                if(!IsRevealed)
+                {
+                    return "";
+                }
+                if(IsBomb)
+                {
+                    return "X";
+                }
+                else
+                {
+                    return SurroundingCount.ToString();
+                }
+            }
+        }
+        public int SurroundingCount { get; set; }
         public int Row { get; set; }
         public int Col { get; set; }
     }
