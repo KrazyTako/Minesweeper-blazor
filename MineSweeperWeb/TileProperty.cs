ae83e7f3674ea877ebf364f5772830cad1e64bff
David Rodriguez <krazytako@hotmail.com>
2020-06-11 22:44:07 -0700

    added media queries and styling for forced box grid

--- MineSweeperWeb/TileProperty.cs
+++ MineSweeperWeb/TileProperty.cs
@@ -1,10 +1,4 @@
-﻿using Microsoft.AspNetCore.SignalR;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
-namespace MineSweeperWeb
+﻿namespace MineSweeperWeb
 {
     public class TileProperty
     {
