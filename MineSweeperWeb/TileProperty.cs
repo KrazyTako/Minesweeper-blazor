61990c2b8cc316c3397288ddd022ed27575646c1
David Rodriguez <krazytako@hotmail.com>
2020-06-13 18:48:16 -0700

    added first turn helper

--- MineSweeperWeb/TileProperty.cs
+++ MineSweeperWeb/TileProperty.cs
@@ -3,6 +3,7 @@
     public class TileProperty
     {
         public bool IsBomb { get; set; }
+        public bool IsFirstHelper { get; set; }
         public bool IsRevealed { get; set; }
         public bool IsFlagged { get; set; }
         public string CssClass
@@ -21,6 +22,10 @@
         {
             get
             {
+                if (IsFirstHelper && !IsRevealed)
+                {
+                    return "oi oi-circle-check";
+                }
                 if (IsFlagged)
                 {
                     return "oi oi-flag";
