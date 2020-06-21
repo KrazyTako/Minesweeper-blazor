7b931c19b20bbe11a05597ad8df3b08c07a6b672
David Rodriguez <krazytako@hotmail.com>
2020-06-21 15:03:40 -0700

    added AppUser score table

--- MineSweeperData/Services/ScoresService.cs
+++ MineSweeperData/Services/ScoresService.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
 using MineSweeperData.Models;
 using System;
 using System.Collections.Generic;
@@ -9,54 +10,81 @@ namespace MineSweeperData.Services
 {
     public class ScoresService
     {
-        readonly MinesweeperDbContext dbContext;
-        public ScoresService(MinesweeperDbContext context)
+        readonly IServiceScopeFactory _serviceScopeFactory;
+        public ScoresService(IServiceScopeFactory serviceScopeFactory)
         {
-            dbContext = context;
+            _serviceScopeFactory = serviceScopeFactory;
         }
 
         public async Task<IEnumerable<Score>> GetAllScores()
         {
-            return await dbContext.Scores.ToListAsync();
+            using (var scope = _serviceScopeFactory.CreateScope())
+            {
+                var context = scope.ServiceProvider.GetService<MinesweeperDbContext>();
+                return await context.Scores.ToListAsync();
+            }
         }
 
         public async Task<Score> CreateUserScore(string userId, int width, int height, int mineCount, long elapsedMilliseconds)
         {
-            Score newScore = new Score();
-            newScore.ApplicationUserId = userId;
-            newScore.Name = "";
-            newScore.Date = DateTime.Now;
-            newScore.Width = width;
-            newScore.Height = height;
-            newScore.MineCount = mineCount;
-            newScore.Time = TimeSpan.FromMilliseconds(elapsedMilliseconds);
+            using (var scope = _serviceScopeFactory.CreateScope())
+            {
+                var context = scope.ServiceProvider.GetService<MinesweeperDbContext>();
+                Score newScore = new Score();
+                newScore.ApplicationUserId = userId;
+                newScore.Name = "";
+                newScore.Date = DateTime.Now;
+                newScore.Width = width;
+                newScore.Height = height;
+                newScore.MineCount = mineCount;
+                newScore.Time = TimeSpan.FromMilliseconds(elapsedMilliseconds);
 
-            dbContext.Scores.Add(newScore);
-            await dbContext.SaveChangesAsync();
-            return newScore;
+                context.Scores.Add(newScore);
+                await context.SaveChangesAsync();
+                return newScore;
+            }
         }
 
         public async Task<Score> CreateAnonymousScore(string name, string message, int width, int height, int mineCount, long elapsedMilliseconds)
         {
-            Score newScore = new Score();
-            newScore.Date = DateTime.Now;
-            newScore.Name = name;
-            newScore.Message = message;
-            newScore.Width = width;
-            newScore.Height = height;
-            newScore.MineCount = mineCount;
-            newScore.Time = TimeSpan.FromMilliseconds(elapsedMilliseconds);
+            using (var scope = _serviceScopeFactory.CreateScope())
+            {
+                var context = scope.ServiceProvider.GetService<MinesweeperDbContext>();
+                Score newScore = new Score();
+                newScore.Date = DateTime.Now;
+                newScore.Name = name;
+                newScore.Message = message;
+                newScore.Width = width;
+                newScore.Height = height;
+                newScore.MineCount = mineCount;
+                newScore.Time = TimeSpan.FromMilliseconds(elapsedMilliseconds);
 
-            dbContext.Scores.Add(newScore);
-            await dbContext.SaveChangesAsync();
-            return newScore;
+                context.Scores.Add(newScore);
+                await context.SaveChangesAsync();
+                return newScore;
+            }
+        }
+
+        public async Task<IEnumerable<Score>> GetScoresByUserId(string userId)
+        {
+            using (var scope = _serviceScopeFactory.CreateScope())
+            {
+                var context = scope.ServiceProvider.GetService<MinesweeperDbContext>();
+                return await context.Scores
+                                  .Where(score => score.ApplicationUserId == userId)
+                                  .ToListAsync();
+            }
         }
 
         public async Task<IEnumerable<Score>> GetScoresInDateRange(DateTime start, DateTime end)
         {
-            return await dbContext.Scores
+            using (var scope = _serviceScopeFactory.CreateScope())
+            {
+                var context = scope.ServiceProvider.GetService<MinesweeperDbContext>();
+                return await context.Scores
                             .Where(score => score.Date > start && score.Date < end)
                             .ToListAsync();
+            }
         }
     }
 }
