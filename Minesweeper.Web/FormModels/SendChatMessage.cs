f84a3205df70c65f7d3c7575c175335580bc2bab
David Rodriguez <krazytako@hotmail.com>
2020-07-03 18:36:37 -0700

    added chathub message validations

---
+++ Minesweeper.Web/FormModels/SendChatMessage.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Minesweeper.Web.FormModels
+{
+    public class SendChatMessage
+    {
+        [Required(AllowEmptyStrings = false)]
+        [MaxLength(255, ErrorMessage = "Message length must be less than 256 characters.")]
+        public string Message { get; set; }
+    }
+}
